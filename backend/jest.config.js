fd2dc872ad8603cfa55cb92a90a67e7fc7ccd12e
Wes <wesbillman@users.noreply.github.com>
2024-09-09 16:20:51 -0700

    chore(vitest): add vitest for testing on backend

--- backend/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  rootDir: 'src',
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-}
